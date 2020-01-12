3b4d99d0904b1ffcb03b68f186890a8f431e4927
PeterisZevalds <54576938+PeterisZevalds@users.noreply.github.com>
2020-01-12 15:43:38 +0200

    First Commit. Nearly done main page.

--- src/Main/java/Main.java
+++ src/Main/java/Main.java
@@ -1,20 +1,27 @@
 import hibernate.CustomerService;
 import hibernate.entities.Customer;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.CustomMenuItem;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.layout.HBox;
+import javafx.stage.Stage;
 
 import java.util.ArrayList;
 
-public class Main  { //extends Application
+public class Main extends Application {
 
-//    @Override
-//    public void start(Stage primaryStage) throws Exception{
-//        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-//        primaryStage.setTitle("CRM");
-//        primaryStage.setScene(new Scene(root, 300, 275));
-//        primaryStage.show();
-//    }
+    public void start(Stage primaryStage) throws Exception {
+        //Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        Scene scene = setupUI(500, 300);
+        primaryStage.setTitle("Simple application");
+        // primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
 
-
-    public static void main(String[] args) {
+    /*public static void main(String[] args) {
 
         CustomerService customerService = new CustomerService();
 //        customerService.createCustomer();
@@ -22,5 +29,75 @@ public class Main  { //extends Application
         Customer customer = customerService.customerById(1);
         System.out.println(customer.toString());
 
+    }*/
+
+    private Scene setupUI(int width, int height) {
+        HBox menuTaskBar = new HBox();
+
+        Menu menuDropdown = new Menu("Menu");
+
+        CustomMenuButton menuHomeButton = new CustomMenuButton("Home");
+        CustomMenuItem customMenuItem = new CustomMenuItem();
+        customMenuItem.setContent(menuHomeButton);
+        customMenuItem.setHideOnClick(false);
+        menuDropdown.getItems().add(customMenuItem);
+
+        CustomMenuButton menuCustomersButton = new CustomMenuButton("Customers");
+        CustomMenuItem customMenuItem2 = new CustomMenuItem();
+        customMenuItem2.setContent(menuCustomersButton);
+        customMenuItem2.setHideOnClick(false);
+        menuDropdown.getItems().add(customMenuItem2);
+
+        CustomMenuButton menuContactsButton = new CustomMenuButton("Contacts");
+        CustomMenuItem customMenuItem3 = new CustomMenuItem();
+        customMenuItem3.setContent(menuContactsButton);
+        customMenuItem3.setHideOnClick(false);
+        menuDropdown.getItems().add(customMenuItem3);
+
+        CustomMenuButton menuEventsButton = new CustomMenuButton("Events");
+        CustomMenuItem customMenuItem4 = new CustomMenuItem();
+        customMenuItem4.setContent(menuEventsButton);
+        customMenuItem4.setHideOnClick(false);
+        menuDropdown.getItems().add(customMenuItem4);
+
+        CustomMenuButton menuTasksButton = new CustomMenuButton("Tasks");
+        CustomMenuItem customMenuItem5 = new CustomMenuItem();
+        customMenuItem5.setContent(menuTasksButton);
+        customMenuItem5.setHideOnClick(false);
+        menuDropdown.getItems().add(customMenuItem5);
+
+        MenuBar menuBar = new MenuBar();
+        menuBar.getMenus().add(menuDropdown);
+        menuTaskBar.getChildren().addAll(menuBar);
+
+        ////////
+        /*HBox createTaskBar = new HBox();
+        Menu createDropdownMenu = new Menu("Create");
+
+        CustomMenuButton createCustomersButton = new CustomMenuButton("Customers");
+        CustomMenuItem createMenuItem = new CustomMenuItem();
+        createMenuItem.setContent(createCustomersButton);
+        createMenuItem.setHideOnClick(false);
+        createDropdownMenu.getItems().add(createMenuItem);
+
+        CustomMenuButton createEventsButton = new CustomMenuButton("Events");
+        CustomMenuItem createMenuItem2 = new CustomMenuItem();
+        createMenuItem2.setContent(createEventsButton);
+        createMenuItem2.setHideOnClick(false);
+        createDropdownMenu.getItems().add(createMenuItem2);
+
+        CustomMenuButton createTasksButton = new CustomMenuButton("Tasks");
+        CustomMenuItem createMenuItem3 = new CustomMenuItem();
+        createMenuItem3.setContent(createTasksButton);
+        createMenuItem3.setHideOnClick(false);
+        createDropdownMenu.getItems().add(createMenuItem3);
+
+        MenuBar createBar = new MenuBar();
+        createBar.getMenus().add(menuDropdown);
+        createTaskBar.getChildren().addAll(createBar);*/
+
+        Scene scene = new Scene(menuTaskBar, width, height);
+        return scene;
     }
+
 }
